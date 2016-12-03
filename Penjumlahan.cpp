504b3357bfe23a3078478ee9d76e83622c8b53ef
DindaSalSal <dindasalsabila9816@gmail.com>
2016-12-03 10:50:53 +0700

    git bash

---
+++ Penjumlahan.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <conio.h>
+# include <iostream.h>
+main( )
+{
+int nilai1,nilai2, total;
+clrscr( );
+
+cout<<"Masukan Nilai 1 : ";
+cin>>nilai1;
+
+cout<<"Masukan Nilai 2 : ";
+cin>>nilai2;
+
+total=nilai1+nilai2;
+cout<<"\nHasil Penjumlahan  Nilai 1 dan 2 adalah = "<<total<<endl;
+getch( );
+}
